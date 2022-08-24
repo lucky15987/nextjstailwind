5ccef93796e07ca009c97c9a01dc859b9c1b230e
Ethan N <lucky35789@yahoo.com>
2022-08-24 16:35:24 -0400

    added red background for hello world

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline bg-red-500">
       Hello world!
     </h1>
   )
